e3c893aad35334d2d377dd7e2fee345673190f1f
barbuium <bar.buium@gmail.com>
2024-11-17 10:38:15 +0200

    Bill3 fixed

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
         String name3 = args[2];
         int totalBill = Integer.parseInt(args[3]);
         double amount = Math.ceil(totalBill / 3.0);
-        System.out.println("dear " + name1 + " " + name2 + " " + "and " + name3 + " " + "pay " + amount + " shekels each.");
+        System.out.println("dear " + name3 + " " + name2 + " " + "and " + name1 + " " + "pay " + amount + " shekels each.");
     }
 }
