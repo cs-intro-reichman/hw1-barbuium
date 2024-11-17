166aa3a30af3c0ba7b74d04d9cf370fe2a380145
barbuium <bar.buium@gmail.com>
2024-11-17 11:02:21 +0200

    FVcalc fixed

--- FVCalc.java
+++ FVCalc.java
@@ -1,16 +1,12 @@
 public class FVcalc {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: java FVCalc <currentValue> <interestRate> <years>");
-            return;
-        }
+       
         int currentValue = Integer.parseInt(args [0]);
         double rate = Double.parseDouble(args [1]);
         int n = Integer.parseInt(args [2]);
 
         double futureValue = currentValue * Math.pow(1 + rate / 100, n);
-        System.out.println("After $d years, a $%d saved at %.1f%% will yield $%d\n", n,currentValue, rate, (int) futureValue);
-        
+        System.out.println("After " + n + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);  
 
     }
 
