3007742b42db03b85486ce0d0be3ba7f2ef7f950
barbuium <bar.buium@gmail.com>
2024-11-17 17:39:57 +0200

    TimeFormet fixed

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,24 +1,26 @@
 public class TimeFormat {
+
         public static void main(String[] args) {
             String time = args[0];
-            String[] timeParts = time.split(":");
-            int hour = Integer.parseInt(timeParts[0]);  
-            int minute = Integer.parseInt(timeParts[1]);
-            String amPm = "AM";
+            String[] timePart = time.split(":");
+            int hour = Integer.parseInt(timePart[0]);  
+            int minute = Integer.parseInt(timePart[1]);
+            String ampm = (hour < 12) ? "AM" : "PM";
             
-
             if (hour == 0) {
                 hour = 12;
-                amPm = "AM";
-            } else if (hour >= 12) {
-                if (hour > 12) {
+            } else if (hour > 12) {
                     hour -= 12;
-                }
-                amPm = "PM";
-            }
-            
-            String minuteFormatted = (minute < 10) ? "0" + minute : Integer.toString(minute);
-            System.out.println(hour + ":" + minuteFormatted + " " + amPm);
+                  }
+
+              String formattedMinute;
+                  if (minute < 10) {
+                      formattedMinute = "0" + minute;  
+                  } else {
+                      formattedMinute = String.valueOf(minute); 
+                  }
+
+                  System.out.println(hour + ":" + formattedMinute + " " + ampm);
         }
     }
 
